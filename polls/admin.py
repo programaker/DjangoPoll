8f4913ee14ec58fdf3c0fe876451c274b75c9302
programaker <programaker@gmail.com>
2011-07-24 12:47:26 -0300

    correcting encoding bug + adding fieldsets to polls form + making date information collapsable

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from polls.models import Poll, Choice
 from django.contrib import admin
 
 
 class PollAdmin(admin.ModelAdmin):
-  fields = ['pub_date', 'question']
+  fieldsets = [
+    ('Principal', {'fields': ['question']}),
+    ('Informações de data', {'fields': ['pub_date'], 'classes': ['collapse']}),
+  ]
 
 
 admin.site.register(Poll, PollAdmin)
