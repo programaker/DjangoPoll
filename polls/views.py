308a2fe947afe45fcd0e8fe4cd9483dc4e63bc7f
programaker <programaker@gmail.com>
2011-07-24 16:14:50 -0300

    creating the poll/index view

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from polls.models import Poll
+
+
+def index(request):
+  latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+  return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+
+def detail(request, poll_id):
+  return HttpResponse("You're looking at poll %s." % poll_id)
+
+
+def results(request, poll_id):
+  return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+
+def vote(request, poll_id):
+  return HttpResponse("You're voting on poll %s." % poll_id)
--- urls.py
+++ urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'DjangoPoll.views.home', name='home'),
-    # url(r'^DjangoPoll/', include('DjangoPoll.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+  (r'^polls/$', 'polls.views.index'),
+  (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+  (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+  (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+  url(r'^admin/', include(admin.site.urls)),
 )
