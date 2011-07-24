ead9a53468e59f54a3ed0d324c4cdc37b67d098d
programaker <programaker@gmail.com>
2011-07-24 13:01:55 -0300

    making polls form inline with choices - master/detail

--- polls/admin.py
+++ polls/admin.py
@@ -4,12 +4,18 @@ from polls.models import Poll, Choice
 from django.contrib import admin
 
 
+class ChoiceInline(admin.StackedInline):
+  model = Choice
+  extra = 0
+
+
 class PollAdmin(admin.ModelAdmin):
   fieldsets = [
     ('Principal', {'fields': ['question']}),
     ('Informações de data', {'fields': ['pub_date'], 'classes': ['collapse']}),
   ]
 
+  inlines = [ChoiceInline]
+
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
