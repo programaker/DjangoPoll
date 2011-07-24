d8831463e9cb4108f221ca611ea63678500dfa17
programaker <programaker@gmail.com>
2011-07-24 14:04:51 -0300

    adding a date hierarchy to polls

--- polls/admin.py
+++ polls/admin.py
@@ -19,6 +19,7 @@ class PollAdmin(admin.ModelAdmin):
   list_display = ('question', 'pub_date', 'was_published_today')
   list_filter = ['pub_date']
   search_fields = ['question']
+  date_hierarchy = 'pub_date'
 
 
 admin.site.register(Poll, PollAdmin)
